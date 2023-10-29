2d391dc00199a0d6dcb02062b5d71dfcf68d4573
karpot <otto.karppinen@outlook.com>
2023-10-29 10:26:24 +0200

    second commit

--- jsSample.js
+++ jsSample.js
@@ -1,5 +1,23 @@
-function changeTextAndColor() {
-    const para = document.getElementById('demoParagraph');
-    para.textContent = "The text has changed!";
-    para.style.color = "blue";
-}
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('******************Hello World*******************');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+
+
+
+//function changeTextAndColor() {
+//    const para = document.getElementById('demoParagraph');
+//    para.textContent = "The text has changed!";
+//    para.style.color = "blue";
+//}
